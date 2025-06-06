64aab95e2c900026356805a23e99a312c3d23af8
Ray Andrade <TheRayCode@RayAndrade.com>
2025-06-06 11:19:14 -0700

    *

--- Behavioral/Observer/JS_pro/index.js
+++ Behavioral/Observer/JS_pro/index.js
@@ -68,22 +68,3 @@ subject.unsubscribe(observerB);
 
 // Trigger second notification: observers A and C will receive "Second message"
 subject.notify("Second message");
-
-// Test code
-/*
-const subject = new Subject();
-
-const observerA = new Observer("Observer A");
-const observerB = new Observer("Observer B");
-const observerC = new Observer("Observer C");
-
-subject.subscribe(observerA);
-subject.subscribe(observerB);
-subject.subscribe(observerC);
-
-subject.notify("First message");
-
-subject.unsubscribe(observerB);
-
-subject.notify("Second message");
-*/
