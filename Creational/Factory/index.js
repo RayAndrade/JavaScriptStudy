4803f0ce5b0255c1d65f5e4ca88f2abfcbf0cc25
Ray Andrade <TheRayCode@RayAndrade.com>
2025-06-15 18:07:54 -0700

    *

--- Creational/Factory/index.js
+++ Creational/Factory/index.js
@@ -1,10 +1,1 @@
-// index.js
-// Purpose: Demo of Factory Method pattern
-
-import ConcreteCreator from './ConcreteCreator.js';
-
-const creator = new ConcreteCreator();
-const result = creator.someOperation();
-
-console.log(result);
-// Expected: "Creator: Working with Result from ConcreteProduct"
+console.log(`TheRayCode is AWESOME!!!`)
