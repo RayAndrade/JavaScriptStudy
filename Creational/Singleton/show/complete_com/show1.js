ff63e66043c63d5e21e7559e828c8f5cd2cf0d88
Ray Andrade <TheRayCode@RayAndrade.com>
2025-06-17 11:22:46 -0700

    *

--- Creational/Singleton/show/complete_com/show1.js
+++ Creational/Singleton/show/complete_com/show1.js
@@ -1,2 +1,46 @@
+class Singleton {
+    constructor() {
+        if (Singleton.instance) {
+            return Singleton.instance;
+        }
+        this.privateRandomNumber = Math.random();
+        Singleton.instance = this;
+    }
+    publicMethod() {
+        console.log("Public method called");
+        this.privateMethod();
+    }
+    getRandomNumber() {
+        return this.privateRandomNumber;
+    }
+    privateMethod() {
+        console.log("Private method called");
+    }
+}
 
-console.log("TheRayCode is AWESOME!!!");
+Singleton.instance = null;
+
+const singletonA = new Singleton();
+const singletonB = new Singleton();
+
+singletonA.publicMethod();
+console.log("Singleton A random number: " + singletonA.getRandomNumber());
+console.log("Singleton B random number: " + singletonB.getRandomNumber());
+
+console.log("Are both instances the same?", singletonA === singletonB);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
