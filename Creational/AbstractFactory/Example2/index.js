c352b2581670d71ca98cacbb9f6fdd3e4f3178e5
Ray Andrade <TheRayCode@RayAndrade.com>
2025-06-20 15:43:38 -0700

    *

--- Creational/AbstractFactory/Example2/index.js
+++ Creational/AbstractFactory/Example2/index.js
@@ -1,1 +1,8 @@
-console.log('TheRayCode is AWESOME!!!')
+const EnchantedMazeFactory = require('./EnchantedMazeFactory');
+const MazeGame = require('./MazeGame');
+
+const factory = new EnchantedMazeFactory();
+const game = new MazeGame();
+const maze = game.createMaze(factory);
+
+console.log(maze.toString());
