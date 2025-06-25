ca80e6ff56363902bb58477481d6d5d4d467dd91
Ray Andrade <TheRayCode@RayAndrade.com>
2025-06-25 13:00:28 -0700

    *

---
+++ Creational/Builder/Example1/Director.js
@@ -0,0 +1,8 @@
+// Director.js
+class Director {
+    construct(builder) {
+        builder.buildPartA();          // Build part A
+        builder.buildPartB();          // Build part B
+    }
+}
+module.exports = Director;
--- Creational/Builder/Example1/index.js
+++ Creational/Builder/Example1/index.js
@@ -1,1 +1,14 @@
-console.log('TheRayCode is AWESOME!!!')
+// index.js
+const ConcreteBuilder = require('./ConcreteBuilder');
+const Director = require('./Director');
+
+// Create builder and director
+const builder = new ConcreteBuilder();
+const director = new Director();
+
+// Director builds the product using the builder
+director.construct(builder);
+
+// Get the final product and show its parts
+const product = builder.getResult();
+product.show();  // → Product parts: PartA, PartB
