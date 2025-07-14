ac3b1d26fef644c8763657ced73afecc89ec07bc
Ray Andrade <TheRayCode@RayAndrade.com>
2025-07-14 10:11:02 -0700

    *

--- Structural/Bridge/Example1/index.js
+++ Structural/Bridge/Example1/index.js
@@ -1,18 +1,18 @@
-import { ConcreteImplementorA } from './ConcreteImplementorA.js';
-import { ConcreteImplementorB } from './ConcreteImplementorB.js';
-import { RefinedAbstraction } from './RefinedAbstraction.js';
+const Composite = require('./Composite');
+const Leaf = require('./Leaf');
+const Client = require('./Client');
 
-// Setup and run
-const implementorA = new ConcreteImplementorA();
-const abstractionA = new RefinedAbstraction(implementorA);
+const root = new Composite("root");
 
-console.log("Calling operation with ConcreteImplementorA:");
-abstractionA.operation();
+const leaf1 = new Leaf("A");
+const leaf2 = new Leaf("B");
 
-console.log("");
+root.add(leaf1);
+root.add(leaf2);
 
-const implementorB = new ConcreteImplementorB();
-const abstractionB = new RefinedAbstraction(implementorB);
+const branch = new Composite("branch");
+branch.add(new Leaf("C1"));
+branch.add(new Leaf("C2"));
 
-console.log("Calling operation with ConcreteImplementorB:");
-abstractionB.operation();
+root.add(branch);
+Client.execute(root);
