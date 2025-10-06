0a1af271b79aaf93428a61991feb38e70814d1e7
Ray Andrade <TheRayCode@RayAndrade.com>
2025-10-06 12:30:00 -0700

    *

--- Behavioral/State/Example1/Context.js
+++ Behavioral/State/Example1/Context.js
@@ -1,19 +1,14 @@
-// Context.js
 
-// Context maintains a reference to the current state
 class Context {
     constructor(state) {
-        this.state = state; // start with an initial state
+        this.state = state;
     }
-
     setState(state) {
-        this.state = state; // update the current state
+        this.state = state;
         console.log(`Context: changed state to ${state.constructor.name}`);
     }
-
     request() {
         this.state.handle(this); // delegate to the current state's handle
     }
 }
-
 module.exports = Context;
--- Behavioral/State/Example1/index.js
+++ Behavioral/State/Example1/index.js
@@ -1,7 +1,4 @@
-// index.js
-
 const Client = require('./Client');
-
 // run the State pattern demo
 Client.run();
 
